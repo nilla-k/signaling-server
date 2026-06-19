4909b6a46224af608297cf8cb0f8217e210f0bb2
nilla-k <nillapria@gmail.com>
2026-06-19 16:13:41 +1000

    update player id to not start with 0

--- src/services/models/Player.ts
+++ src/services/models/Player.ts
@@ -1,7 +1,10 @@
 import WebSocket from 'ws'
 
 export class Player {
-	readonly id: string = Math.random().toString().substring(2, 10)
+	readonly id: string = (
+		Math.floor(Math.random() * 90000000) + 10000000
+	).toString();
+	
 	socket: WebSocket
 
 	constructor(socket: WebSocket) {
