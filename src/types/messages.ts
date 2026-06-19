6f89fdaa6f32aea758f0ad27990fa02111c31312
nilla-k <nillapria@gmail.com>
2026-06-19 16:14:34 +1000

    update message types

--- src/types/messages.ts
+++ src/types/messages.ts
@@ -1,14 +1,24 @@
+/**
+ * These values must align with the same number and order of values
+ * defined in the Godot client. 
+ * 
+ * Because of the numbers, you see. Pesky numbers.
+ */
 export enum MessageType {
-	CreateRoom = 'create_room',
-	JoinRoom = 'join_room',
-	GetStatus = 'get_status',
+	ConnectionStart = 0,
+	CreateRoom = 1,
+	RoomCreated = 2,
+	JoinRoom = 3,
+	GetStatus = 4, // Can probs delete
+	PlayerConnected = 5,
+	JoinedRoom = 6,
+	Offer = 7,
+	Answer = 8,
+	Candidate = 9,
+	Error = 10,
 }
 
 export type Message = {
 	type: MessageType
-	body: string
-}
-
-export type JoinRoomMessageBody = {
-	roomId: string
+	data: object
 }
