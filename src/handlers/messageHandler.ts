31d6c73ffd2c4085d4c668f8b4f7cd63378b164f
nilla-k <nillapria@gmail.com>
2026-06-19 16:15:15 +1000

    handle rtc connection messages

--- src/handlers/messageHandler.ts
+++ src/handlers/messageHandler.ts
@@ -1,5 +1,5 @@
 import { RawData } from 'ws'
-import { MessageType } from '../types/messages.js'
+import { Message, MessageType } from '../types/messages.js'
 import { Either, left, right } from '../types/utils.js'
 import { roomManager } from '../services/RoomManager.js'
 import { Player } from '../services/models/Player.js'
@@ -16,26 +16,64 @@ export const handleMessage = (
 				return roomManager.createRoom(player)
 
 			case MessageType.JoinRoom: {
-				const roomId = message.body?.id
+				const roomId = message.data?.id
 				if (roomId) {
 					const errorOrRoom = roomManager.joinRoom(roomId, player)
 					if (errorOrRoom.tag === 'right') {
 						const room = errorOrRoom.value
+
 						room.players.forEach((p) => {
+							// send new player info to existing players
+							const newPlayerMessage: Message = {
+								type: MessageType.PlayerConnected,
+								data: {
+									id: player.id,
+									room: room.id
+								}
+							}
 							if (p.id !== player.id) {
-								p.socket.send(`Player ${player.id} joined room ${room.id}`)
+								p.socket.send(JSON.stringify(newPlayerMessage))
+							}
+							
+							// send existing player info to new player
+							const existingPlayersMessage: Message = {
+								type: MessageType.PlayerConnected,
+								data: {
+									id: p.id,
+									room: room.id
+								}
+							}
+							if (p.id !== player.id) {
+								player.socket.send(JSON.stringify(existingPlayersMessage))
 							}
 						})
+
 						return right('Joined successfully')
 					} else {
 						return errorOrRoom
 					}
 				} else {
-					return left(Error('Room id missing from message body'))
+					return left(Error('Room ID missing from message body'))
 				}
 			}
 			case MessageType.GetStatus:
 				return right(roomManager.getStatus(player))
+
+			case MessageType.Answer:
+			case MessageType.Candidate:
+			case MessageType.Offer: {
+				// Pass data directly between peers for any message for establishing RTC connection
+				const room = roomManager.getRoom(message.roomId)
+				const targetPlayer = room?.players.find((p) => p.id === message.peer)
+
+				if (targetPlayer) {
+					targetPlayer.socket.send(JSON.stringify(message))
+				} else {
+					return left(Error("Error finding peer"))
+				}
+
+				return right("")
+			}
 			default:
 				return left(Error('Received unknown or missing message type.'))
 		}
